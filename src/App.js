5da9977b67929ed748fec023032f4e0147d77e80
AlshehriAbdullah <37164419+AlshehriAbdullah@users.noreply.github.com>
2025-08-27 10:35:56 +0300

    Update Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Page components
+import Home from './pages/home/Home'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+import Create from './pages/create/Create'
+
+//Styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cooking Recipes</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/recipe/:id' element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
